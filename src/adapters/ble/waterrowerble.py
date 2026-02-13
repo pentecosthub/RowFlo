fdc71cd02ce6b79f9b8beba4f152f35c3dae0702
pentecosthub <openheavens@gmail.com>
2026-02-12 21:27:47 -0500

    Change BLE broadcast name from PiRowFlo to RowFlo

--- src/adapters/ble/waterrowerble.py
+++ src/adapters/ble/waterrowerble.py
@@ -438,7 +438,7 @@ class FTMPAdvertisement(Advertisement):
         self.add_service_uuid(HeartRate.HEART_RATE)
 
         #self.add_local_name("S4 Comms PI")
-        self.add_local_name("PiRowFlo")
+        self.add_local_name("RowFlo")
         self.include_tx_power = True
 
 
